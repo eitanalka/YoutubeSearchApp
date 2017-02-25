0492767fa79dc9a82a4283e31208905985776bda
Eitan Alkaslassy <eitanalka@gmail.com>
2017-02-25 16:34:06 -0500

    changed mobile layout

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -12,7 +12,7 @@ const VideoList = (props) => {
   });
 
   return (
-    <ul className="col-md-4 list-group">
+    <ul className="col-md-4 col-sm-8 list-group">
       {videoItems}
     </ul>
   );
